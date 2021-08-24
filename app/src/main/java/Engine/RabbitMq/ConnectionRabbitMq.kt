e945041ce70c6d9265df1bec6444ecc43426dcd5
Ilyas1994 <70584169+kudaikulov@users.noreply.github.com>
2021-08-24 20:27:43 +0600

    новые добавления

--- app/src/main/java/Engine/RabbitMq/ConnectionRabbitMq.kt
+++ app/src/main/java/Engine/RabbitMq/ConnectionRabbitMq.kt
@@ -1,15 +1,47 @@
 package Engine.RabbitMq
 
-import com.example.messageapp.MainActivity
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
 import com.rabbitmq.client.ConnectionFactory
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-object ConnectionRabbitMq{
-    var factory =  ConnectionFactory()
-    lateinit var mainAct:MainActivity
+class ConnectionRabbitMq: Service() {
+
+
+   private  var rabbitmq: RabbitMq = RabbitMq()
+
     var  sendMes = ""
-    init {
-        factory.host = "192.168.0.108"
-        factory.username ="admin"
-        factory.password = "admin"
+        companion object{
+             private var instance:ConnectionRabbitMq? = null
+                fun GetInstance() = synchronized(this) {
+                    if(instance == null)
+                        instance = ConnectionRabbitMq()
+                    instance
+                }
+        }
+
+
+
+        fun SetRecyclerViewAdapter(adapter:IRecyclerViewDispatchUpdatesTo){
+            rabbitmq.SetRecyclerViewAdapter(adapter)
+        }
+        fun CreateConnection(host:String, username:String, pass:String){
+            GlobalScope.launch {
+                var  factory =  ConnectionFactory()
+                factory.host = host
+                factory.username =username
+                factory.password = pass
+                var connection= factory.newConnection()
+                rabbitmq.createChannel(connection)
+            }
+        }
+    override fun onCreate() {
+       CreateConnection("192.168.0.108","admin","admin")
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
     }
 }
